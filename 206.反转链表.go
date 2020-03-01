504251f45691cd02747260029c0c79d61b554daf
baronliu <baronliu@tencent.com>
2020-03-01 11:21:42 +0800

    翻转链表

---
+++ 206.反转链表.go
@@ -0,0 +1,53 @@
+/*
+ * @lc app=leetcode.cn id=206 lang=golang
+ *
+ * [206] 反转链表
+ *
+ * https://leetcode-cn.com/problems/reverse-linked-list/description/
+ *
+ * algorithms
+ * Easy (62.73%)
+ * Likes:    785
+ * Dislikes: 0
+ * Total Accepted:    165.8K
+ * Total Submissions: 246.3K
+ * Testcase Example:  '[1,2,3,4,5]'
+ *
+ * 反转一个单链表。
+ *
+ * 示例:
+ *
+ * 输入: 1->2->3->4->5->NULL
+ * 输出: 5->4->3->2->1->NULL
+ *
+ * 进阶:
+ * 你可以迭代或递归地反转链表。你能否用两种方法解决这道题？
+ *
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+func reverseList(head *ListNode) *ListNode {
+	var prev *ListNode
+	for head != nil {
+		if head.Next == nil {
+			head.Next = prev
+			break
+		} else {
+			tmp := head.Next
+			head.Next = prev
+			prev = head
+			head = tmp
+		}
+	}
+	return head
+}
+
+// @lc code=end
+
