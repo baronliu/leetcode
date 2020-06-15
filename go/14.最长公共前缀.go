5c0e00b3c7152c512f4bcb6e19a7d7d1f5092818
baronliu <baronliu@tencent.com>
2020-06-15 23:55:35 +0800

    14_最长公共前缀_GO_优化写法

--- go/14.最长公共前缀.go
+++ go/14.最长公共前缀.go
@@ -34,30 +34,39 @@
  *
  */
 func longestCommonPrefix(strs []string) string {
-	if len(strs) < 1 {
-		return ""
+    //空数组
+    if len(strs) < 1 {
+        return ""
+    }
+    //单元素数组结果为其本身
+    if len(strs) == 1 {
+        return strs[0]
+    }
+    //两两比较到最后一组
+    prev := strs[0]
+    for i:=1; i<len(strs); i++ {
+        //如果比较而得的元素为空，则停止比较
+        if len(prev) == 0 {
+            return ""
+        }
+        prev = compare(prev, strs[i])
+    }
+    return prev
+}
+
+//求两个字符串的公共部分
+func compare(str1, str2 string) string {
+	l := len(str1)
+	if l > len(str2) {
+		l = len(str2)
 	}
-
-	result := strs[0]
-
-	for i := 1; i < len(strs); i++ {
-		if result == "" {
+	i := 0
+	for i < l {
+		if str1[i] != str2[i] {
 			break
 		}
-		j := 0
-		newResult := ""
-		for j < len(strs[i]) && j < len(result) {
-
-			if strs[i][j] == result[j] {
-				newResult = newResult + string(result[j])
-			} else {
-				break
-			}
-			j++
-		}
-		result = newResult
+		i++
 	}
-
-	return result
+	return str1[0:i]
 }
 
