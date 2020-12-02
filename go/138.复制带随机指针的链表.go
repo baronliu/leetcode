761bd3cf76f883a8e6ad86c6c907f8926cf54917
baronliu <baronliu@tencent.com>
2020-12-02 11:24:37 +0800

    138_复制带随机指针的链表_GO

--- go/138.复制带随机指针的链表.go
+++ go/138.复制带随机指针的链表.go
@@ -14,35 +14,37 @@
  * }
  */
 
-/**
- * 思路：先不管随机指针的属性，就当是复制一个普通的链表，那么问题核心就是随机指针的复制
- * 对于随机指针的复制，核心问题点是我们不能提前知道目标地址，那么理所当然考虑两次遍历
- * 第一次遍历的时候记录下每个节点的新地址和旧地址的隐射关系，第二次遍历把旧的随机节点替换成新的随机节点即可
- */
 func copyRandomList(head *Node) *Node {
-	cur := head
-	res := new(Node)
-	p := res
+	if head == nil {
+		return nil
+	}
 	dict := make(map[*Node]*Node)
-	for cur != nil {
-		v := &Node{
-			Val: cur.Val,
+
+	temp := head
+	prev := &Node{}
+	retNode := prev
+	for temp != nil {
+		n := getCloneNode(dict, temp)
+		n.Random = getCloneNode(dict, temp.Random)
+		prev.Next = n
+		prev = n
+		temp = temp.Next
+	}
+
+	return retNode.Next
+}
+
+func getCloneNode(dict map[*Node]*Node, node *Node) *Node {
+	if newNode, ok := dict[node]; ok {
+		return newNode
+	} else {
+		var newNode *Node
+		if node != nil {
+			newNode = &Node{Val: node.Val}
 		}
-		dict[cur] = v
-		p.Next = v
-		p = p.Next
-		cur = cur.Next
+		dict[node] = newNode
+		return newNode
 	}
-
-	cur = head
-	p = res.Next
-	for cur != nil {
-		p.Random = dict[cur.Random]
-		p = p.Next
-		cur = cur.Next
-	}
-
-	return res.Next
 }
 
 // @lc code=end
