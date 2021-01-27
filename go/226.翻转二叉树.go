5cef904dd728620db95fb70477d75825dbb1618e
baronliu <baronliu@tencent.com>
2021-01-27 13:13:47 +0800

    226_翻转二叉树_GO

---
+++ go/226.翻转二叉树.go
@@ -0,0 +1,31 @@
+/*
+ * @lc app=leetcode.cn id=226 lang=golang
+ *
+ * [226] 翻转二叉树
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func invertTree(root *TreeNode) *TreeNode {
+	if root == nil {
+		return nil
+	}
+
+	left := root.Left
+	right := root.Right
+
+	root.Left = invertTree(right)
+	root.Right = invertTree(left)
+
+	return root
+}
+
+// @lc code=end
+
