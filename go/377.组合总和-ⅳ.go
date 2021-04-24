a829d91989a969615d15a0353bf17973cbd9be88
baronliu <baronliu@tencent.com>
2021-04-24 12:59:50 +0800

    377_组合总和_IV_GO

---
+++ go/377.组合总和-ⅳ.go
@@ -0,0 +1,25 @@
+/*
+ * @lc app=leetcode.cn id=377 lang=golang
+ *
+ * [377] 组合总和 Ⅳ
+ */
+
+// @lc code=start
+func combinationSum4(nums []int, target int) int {
+	dp := make([]int, target+1)
+	dp[0] = 1
+	for i := 1; i <= target; i++ {
+		for _, num := range nums {
+			if i < num {
+				continue
+			}
+
+			dp[i] += dp[i-num]
+		}
+	}
+
+	return dp[target]
+}
+
+// @lc code=end
+
